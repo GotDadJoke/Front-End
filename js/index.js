578feb6c3ca073475016dbc6f913a8c85456de6c
charlesmsy <charles.m.sy@hotmail.com>
2020-07-21 21:25:45 -0700

    update ui. Color added.

--- js/index.js
+++ js/index.js
@@ -1,10 +1,29 @@
-const api_url = '/random'
+const api_url_random = 'https://ckk6y2se8b.execute-api.us-east-1.amazonaws.com/dev/random'
+const api_url_search = 'https://ckk6y2se8b.execute-api.us-east-1.amazonaws.com/dev/searchbyterm?term='
+var term = 'dog'
 
-function findDadJokes(){
+function findRandomDadJokes(){
 
-    fetch(api_url, { method: "get"})
-    .then(data => {return data.text()})
-    .then(res => {
-        document.getElementById('jokeLabelBtn').innerHTML = res 
-        console.log(res)})
+    fetch(api_url_random)
+        .then(response => {
+            console.log(response); 
+            return response.json();
+    })
+    .then(joke => {
+        console.log(joke);
+        document.getElementById('jokeLabel').innerHTML = joke.joke;
+    })
+}
+
+function searchTermDadJokes(){
+
+    fetch(api_url_random+term)
+        .then(response => {
+            console.log(response); 
+            return response.json();
+    })
+    .then(joke => {
+        console.log(joke);
+        document.getElementById('jokeLabel').innerHTML = joke.joke;
+    })
 }
