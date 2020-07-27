b1db7d4de64abe37150b085e3b826ee6fd8ddeca
charlesmsy <charles.m.sy@hotmail.com>
2020-07-27 13:03:39 -0700

    Add search bar and using team's API.

--- js/index.js
+++ js/index.js
@@ -1,6 +1,9 @@
-const api_url_random = 'https://ckk6y2se8b.execute-api.us-east-1.amazonaws.com/dev/random'
-const api_url_search = 'https://ckk6y2se8b.execute-api.us-east-1.amazonaws.com/dev/searchbyterm?term='
-var term = 'dog'
+//https://api.gotdadjoke.com/search
+
+
+const api_url_random = 'https://api.gotdadjoke.com/randomJoke//'
+const api_url_search = 'https://api.gotdadjoke.com/searchTerm?term='
+//https://cors-anywhere.herokuapp.com/
 
 function findRandomDadJokes(){
 
@@ -17,13 +20,62 @@ function findRandomDadJokes(){
 
 function searchTermDadJokes(){
 
-    fetch(api_url_random+term)
+    var searchTerm = document.getElementById("search").value;
+    var newAPI = api_url_search + searchTerm;
+    
+    fetch(newAPI)
         .then(response => {
-            console.log(response); 
+            console.log(response);
             return response.json();
     })
-    .then(joke => {
-        console.log(joke);
-        document.getElementById('jokeLabel').innerHTML = joke.joke;
+    .then(jsonFile => {
+        console.log(jsonFile);
+        return jsonFile.results;
     })
+    .then(results => { //results = shold be array of jokes
+        console.log(results);
+
+        document.getElementById('searchLabel').innerHTML = "<strong>Results of <strong> '"+ searchTerm + "'." + "<br>" + "<br>";
+
+        for ( var i = 0; i < results.length; i++) {
+            document.getElementById('searchLabel').innerHTML 
+             = document.getElementById('searchLabel').innerHTML + " " + results[i].joke + "<br>" + "<br>";
+        }
+    });
 }
+
+function hidOrDisplayWhenSearch() {
+    var x = document.getElementById("hidMeWhenSearch");
+    var y = document.getElementById("displayMeWhenSearch");
+    var z = document.getElementById("searchLabel");
+    x.style.display = "none";
+    y.style.display = "block";
+    z.style.display = "block";
+}
+
+function reverseHidOrDisplayWhenSearch() {
+    var x = document.getElementById("hidMeWhenSearch");
+    var y = document.getElementById("displayMeWhenSearch");
+    var z = document.getElementById("searchLabel");
+    x.style.display = "block";
+    y.style.display = "none";
+    z.style.display = "none";
+}
+
+function searchBtnFunction() {
+    hidOrDisplayWhenSearch();
+    searchTermDadJokes();
+}
+
+function hiddenRandomJokeBtn() {
+    reverseHidOrDisplayWhenSearch();
+    findRandomDadJokes();
+}
+
+function handleKeyPress(e){
+    var key=e.keyCode || e.which;
+    if (key==13){
+        searchBtnFunction();
+    }
+}
+
